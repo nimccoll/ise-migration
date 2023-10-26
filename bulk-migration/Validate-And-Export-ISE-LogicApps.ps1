f00f5cfc9473a70dc6c8df68a8ffaa6057c0ace4
Nicholas McCollum <nimccoll@microsoft.com>
2023-10-26 08:50:03 -0400

    Added summary information

--- bulk-migration/Validate-And-Export-ISE-LogicApps.ps1
+++ bulk-migration/Validate-And-Export-ISE-LogicApps.ps1
@@ -50,43 +50,98 @@ Get-AzResource -ResourceGroupName $resourceGroupName -ResourceType 'Microsoft.Lo
 }
 
 # Validate and Export each Logic App from the specified ISE
+$validateSucceededCount = 0
+$validateFailedCount = 0
+$validateFailed = @()
+$exportSucceededCount = 0
+$exportFailedCount = 0
+$exportFailed = @()
 $logicApps | ForEach-Object {
     $body = '{"properties":{"workflows":[{"id":"' + $_ + '"}],"workflowExportOptions":""}}'
-    $validateResponse = Invoke-WebRequest -UseBasicParsing $validateUrl -Headers $head -ContentType 'application/json' -Method POST -Body $body
-    if ($validateResponse.StatusCode -eq '200') {
-        $validateResponseContent = ConvertFrom-Json -InputObject $validateResponse.Content
-        Write-Host $validateResponseContent.properties.workflows.PSObject.Properties.Name 'Validated successfully' -ForegroundColor Green
-        Write-Host 'Details'
-        Write-Host '======='
-        $validateResponseContent.properties.workflows.PSObject.Properties.Value | ConvertTo-Json
-        $exportResponse = Invoke-WebRequest -UseBasicParsing $exportUrl -Headers $head -ContentType 'application/json' -Method POST -Body $body
-        if ($exportResponse.StatusCode -eq '200') {
-            $exportResponseContent = ConvertFrom-Json -InputObject $exportResponse.Content
-            Write-Host $validateResponseContent.properties.workflows.PSObject.Properties.Name 'Exported successfully' -ForegroundColor Green
+    try {
+        $currentLogicApp = $_
+        $validateResponse = Invoke-WebRequest -UseBasicParsing $validateUrl -Headers $head -ContentType 'application/json' -Method POST -Body $body
+        if ($validateResponse.StatusCode -eq '200') {
+            $validateSucceededCount = $validateSucceededCount + 1
+            $validateResponseContent = ConvertFrom-Json -InputObject $validateResponse.Content
+            Write-Host $validateResponseContent.properties.workflows.PSObject.Properties.Name 'Validated successfully' -ForegroundColor Green
             Write-Host 'Details'
             Write-Host '======='
-            Write-Host 'Package Link:' $exportResponseContent.properties.packageLink.uri
-            Write-Host
-            $exportResponseContent.properties.details | ForEach-Object {
-                Write-Host $_.exportDetailCategory $_.exportDetailCode $_.exportDetailMessage -ForegroundColor Yellow
+            $validateResponseContent.properties.workflows.PSObject.Properties.Value | ConvertTo-Json
+            try {
+
+                $exportResponse = Invoke-WebRequest -UseBasicParsing $exportUrl -Headers $head -ContentType 'application/json' -Method POST -Body $body
+                if ($exportResponse.StatusCode -eq '200') {
+                    $exportSucceededCount = $exportSucceededCount + 1
+                    $exportResponseContent = ConvertFrom-Json -InputObject $exportResponse.Content
+                    Write-Host $validateResponseContent.properties.workflows.PSObject.Properties.Name 'Exported successfully' -ForegroundColor Green
+                    Write-Host 'Details'
+                    Write-Host '======='
+                    Write-Host 'Package Link:' $exportResponseContent.properties.packageLink.uri
+                    Write-Host
+                    $exportResponseContent.properties.details | ForEach-Object {
+                        Write-Host $_.exportDetailCategory $_.exportDetailCode $_.exportDetailMessage -ForegroundColor Yellow
+                    }
+                    Write-Host
+                }
+                else {
+                    $exportFailedCount = $exportFailedCount + 1
+                    $exportFailed += $currentLogicApp
+                    Write-Host $_ 'Export failed' -ForegroundColor Red
+                    Write-Host 'Details' -ForegroundColor Red
+                    Write-Host '=======' -ForegroundColor Red
+                    Write-Host 'Status Code:' $exportResponse.StatusCode -ForegroundColor Red
+                    Write-Host 'Content:' $exportResponse.Content -ForegroundColor Red
+                    Write-Host
+                }
             }
-            Write-Host
+            catch {
+                $exportFailedCount = $exportFailedCount + 1
+                $exportFailed += $currentLogicApp
+                Write-Host $_ 'Export failed' -ForegroundColor Red
+                Write-Host 'Details' -ForegroundColor Red
+                Write-Host '=======' -ForegroundColor Red
+                Write-Host $PSItem.ToString() -ForegroundColor Red
+                Write-Host
+            }
         }
         else {
-            Write-Host $_ 'Export failed' -ForegroundColor Red
-            Write-Host 'Details'
-            Write-Host '======='
-            Write-Host 'Status Code:' $exportResponse.StatusCode
-            Write-Host 'Content:' $exportResponse.Content
+            $validateFailedCount += $validateFailedCount + 1
+            $validateFailed += $currentLogicApp
+            Write-Host $_ 'Validation failed' -ForegroundColor Red
+            Write-Host 'Details' -ForegroundColor Red
+            Write-Host '=======' -ForegroundColor Red
+            Write-Host 'Status Code:' $validateResponse.StatusCode -ForegroundColor Red
+            Write-Host 'Content:' $validateResponse.Content -ForegroundColor Red
             Write-Host
         }
     }
-    else {
-        Write-Host $_ 'Validation failed' -ForegroundColor Red
-        Write-Host 'Details'
-        Write-Host '======='
-        Write-Host 'Status Code:' $validateResponse.StatusCode
-        Write-Host 'Content:' $validateResponse.Content
-        Write-Host
+    catch {
+            $validateFailedCount += $validateFailedCount + 1
+            $validateFailed += $currentLogicApp
+            Write-Host $_ 'Validation failed' -ForegroundColor Red
+            Write-Host 'Details' -ForegroundColor Red
+            Write-Host '=======' -ForegroundColor Red
+            Write-Host $PSItem.ToString() -ForegroundColor Red
+            Write-Host
+    }
+}
+Write-Host 'Logic Apps successfully validated:' $validateSucceededCount -ForegroundColor Green
+Write-Host 'Logic Apps that failed validation:' $validateFailedCount -ForegroundColor Red
+Write-Host 'Logic Apps successfully exported:' $exportSucceededCount -ForegroundColor Green
+Write-Host 'Logic Apps that failed export:' $exportFailedCount -ForegroundColor Red
+Write-Host
+if ($validateFailedCount -gt 0) {
+    Write-Host 'Logic Apps that failed validation'
+    Write-Host '================================='
+    $validateFailed | ForEach-Object {
+        Write-Host $_
+    }
+}
+if ($exportFailedCount -gt 0) {
+    Write-Host 'Logic Apps that failed export'
+    Write-Host '============================='
+    $exportFailed | ForEach-Object {
+        Write-Host $_
     }
 }
